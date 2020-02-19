6bdce9d31f709d36591b9d1d650c5f05c258833c
CCreek96 <creek.connor@yahoo.com>
2020-02-19 10:45:46 -0600

    cleaned up print statements

--- docker_prune.sh
+++ docker_prune.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 function docker_prune() {
 	local option=$1
 	local commands=$2
-	echo "$option commands: $commands"
 
 	# Ensure volumes command is only passed to system option
 	if [[ "$commands" =~ "--volumes" ]]; then
 		if [[ "$option" == "system" ]]; then
-			echo "running prune:"
-			echo "docker $option prune -f $commands"
+			echo "running prune: docker $option prune -f $commands"
 			docker $option prune -f $commands
-			echo
+			#echo 
 		else
 			echo "WARN: the --volumes command is only allowed with system option"
 			echo "removing the offending command and retrying"
-			echo "old commands: $commands"
+			#echo "old commands: $commands"
 			commands="${commands/" --volumes"/""}"
 			echo "new commands: $commands"
+			echo
 			docker_prune "$option" "$commands"
 		fi
 	# Ensure all command is only passed to system option
 	elif [[ "$commands" =~ "-a" ]]; then
 		if [[ "$option" == "system" ]] || [[ "$option" == "image" ]]; then
-			echo "running prune: "
-			echo "docker $option prune -f $commands"
+			echo "running prune: docker $option prune -f $commands"
 			docker $option prune -f $commands
-			echo 
+			#echo 
 		else
 			echo "WARN: the --all & -a commands are only allowed with system or image options"
 			echo "removing the offending command and retrying"
-			echo "old commands: $commands"
+			#echo "old commands: $commands"
 			if [[ "$commands" =~ "--all" ]]; then
 				commands="${commands/" --all"/""}"
 			fi
@@ -40,20 +38,20 @@ function docker_prune() {
 				commands="${commands/" -a"/""}"
 			fi
 			echo "new commands: $commands"
+			echo 
 			docker_prune "$option" "$commands"
 		fi
 	# allow other commands to run
 	else
-		echo "running prune:"
-		echo "docker $option prune -f $commands"
+		echo "running prune: docker $option prune -f $commands"
 		docker $option prune -f $commands
-		echo 
+		#echo 
 	fi
 }
 
 while true; do
 	usr_cmds="$@"
-	echo "user specified commands: $commands"
+	#echo "user specified commands: $commands"
 	# pass given options or default to system
 	if [ -z "$OPTIONS" ] || [[ "$OPTIONS" =~ "system" ]]; then
 		docker_prune "system" "$usr_cmds"
@@ -67,7 +65,7 @@ while true; do
 	if [ -n "$SLEEP" ]; then
         	echo "Sleeping for $SLEEP seconds..."
         	sleep "$SLEEP"
-        	echo
+        	echo 
     	else
         	exit 0
     	fi
